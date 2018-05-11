7d6380080d4bad53d5f1dc2330ee8172c6482ccf
Clayton Lautier <clayton.lautier@cerberus.io>
2018-05-11 16:14:38 +0200

    packages: downgrade rxjs from 6.1.0 to 5.5.10

--- src/hub-connection.ts
+++ src/hub-connection.ts
@@ -6,7 +6,12 @@ import {
 	HubConnection as SignalRHubConnection,
 	HubConnectionBuilder as SignalRHubConnectionBuilder
 } from "@aspnet/signalr";
-import { from as fromPromise, timer, BehaviorSubject, Observable, Observer } from "rxjs";
+// import { from as fromPromise, timer, BehaviorSubject, Observable, Observer } from "rxjs";
+import { fromPromise } from "rxjs/observable/fromPromise";
+import { timer } from "rxjs/observable/timer";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/Observable";
+import { Observer } from "rxjs/Observer";
 
 import {
 	ConnectionState, ConnectionStatus, HubConnectionOptions,
