dce82c4940cea2d24ce9ae7e7e8d7bf023d37b96
Clayton Lautier <clayton.lautier@outlook.com>
2021-04-30 15:39:40 +0200

    fix

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -12,6 +12,12 @@ ssvTools.registerGulpMultiTargetBuilds({
 	config: config
 });
 
+gulp.task("compile:test", () => ssvTools.compileTsc({
+	module: "es2015",
+	configPath: "./tsconfig.test.json",
+	continueOnError: args.continueOnError
+}));
+
 gulp.task("bundle:ts", () => ssvTools.rollup({ continueOnError: args.continueOnError, useTypeScriptConfig: true }));
 gulp.task("build:resources", () => ssvTools.buildResources(config.output.dist));
 gulp.task("prebuild:rel", () => ssvTools.prepareReleaseBuild({
@@ -33,12 +39,6 @@ gulp.task("build", args.isRelease
 gulp.task("rebuild", gulp.series("clean", "build"))
 
 gulp.task("ci", gulp.series("rebuild", "compile:test"));
-
-gulp.task("compile:test", () => ssvTools.compileTsc({
-	module: "es2015",
-	configPath: "./tsconfig.test.json",
-	continueOnError: args.continueOnError
-}));
 
 // scripts - compile:ts | compile:ts:dev | compile:ts:TARGET
 function compileTs(target) {
