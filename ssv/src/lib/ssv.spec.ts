4fabb6f37c76008bcea07aefc4149b1b9e8c6fbb
Stephen Lautier <stephen.lautier@outlook.com>
2023-08-27 18:37:55 +0200

    nx init workspace - subfolder

---
+++ ssv/src/lib/ssv.spec.ts
@@ -0,0 +1,7 @@
+import { ssv } from './ssv';
+
+describe('ssv', () => {
+	it('should work', () => {
+		expect(ssv()).toEqual('ssv');
+	});
+});
---
+++ ssv/src/lib/ssv.ts
@@ -0,0 +1,3 @@
+export function ssv(): string {
+	return 'ssv';
+}
