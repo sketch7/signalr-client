caefdbf2830cb9bd56f73eb6241db53e3660d9df
Stephen Lautier <stephen.lautier@outlook.com>
2023-09-07 18:24:50 +0200

    change to esbuild + fix folder name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-	cacheDir: './node_modules/.vite/ssv',
+	cacheDir: './node_modules/.vite/signalr-client',
 
 	plugins: [nxViteTsPaths()],
 
